823f85de46cf011f19d288c14b0a5b93d48bec13
FrlFrz <farel.purnomosasi20@gmail.com>
2025-11-22 00:32:16 +0700

    Modifikasi Struktur Database : User, Pemesanan & Jenis Mobil

--- database/migrations/2025_11_07_153244_create_pemesanan_table.php
+++ database/migrations/2025_11_07_153244_create_pemesanan_table.php
@@ -13,17 +13,20 @@ return new class extends Migration
     {
         Schema::create('pemesanan', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('mobil_id')->constrained('mobil');
+            $table->foreignId('id_unit_mobil')->constrained('unit_mobil');
+            $table->foreignId('user_id')->constrained('users');
             $table->string('nama_penyewa');
             $table->string('nik_penyewa', 11);
             $table->string('telepon_penyewa', 15);
             $table->string('alamat_penyewa', 100);
             $table->enum('jaminan_penyewa', ['paspor', 'stnk', 'bpkb', 'sim']);
+            $table->string('foto_ktp_sim');
             $table->dateTime('tgl_mulai');
+            $table->integer('durasi_rental');
             $table->dateTime('tgl_selesai');
             $table->decimal('total_biaya', 10, 2)->default(0);
             $table->uuid('verification_token')->unique();
-            $table->enum('status_pemesanan', ['dipesan', 'sedang_dirental', 'selesai', 'dibatalkan'])
+            $table->enum('status_pemesanan', ['dipesan', 'dirental', 'selesai', 'dibatalkan'])
                   ->default('dipesan');
             $table->foreignId('diserahkan_oleh')->nullable()->constrained('users');
             $table->timestamp('tgl_verifikasi')->nullable();
